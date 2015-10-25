6b15779b8b7448af20b2d8cec776509c121fc1e6
Jake Ruth <jruth@albany.edu>
2015-10-25 12:26:41 -0400

    Cleaned up code and created a separate file for the ProfilePageBody component.

---
+++ Components/Profile/ProfilePageBody.ios.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  View,
+  Text,
+  Image,
+  StyleSheet
+} = React
+
+var styles = StyleSheet.create({
+  profileBodyContent: {
+    flex: 1,
+    backgroundColor: 'rgba(0,124,158,.2)'
+  },
+  fullName: {
+    textAlign: 'center',
+    fontSize: 30,
+    borderWidth: 1,
+    borderColor: '#007C9E',
+    borderRadius: 5,
+    margin: 5,
+    backgroundColor: 'white'
+  },
+  profilePictureContainer: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 5
+  },
+  profileImage: {
+    width: 150,
+    height: 230
+  },
+  fanCount: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: '800'
+  },
+  bio: {
+    flex: 2,
+    alignSelf: 'auto',
+    margin: 30
+  }
+});
+
+var ProfilePageBody = React.createClass({
+
+  propTypes: {
+    firstName: React.PropTypes.string.isRequired,
+    lastName: React.PropTypes.string.isRequired,
+    bio: React.PropTypes.string.isRequired,
+    numFans: React.PropTypes.number.isRequired,
+    profileImageUrl: React.PropTypes.string.isRequired
+  },
+
+  render: function() {
+    var firstName = this.props.firstName,
+        lastName = this.props.lastName,
+        bio = this.props.bio,
+        numFans = this.props.numFans,
+        profileImageUrl = this.props.profileImageUrl;
+
+    return (
+      <View style={styles.profileBodyContent}>
+        <Text style={styles.fullName}>{firstName} {lastName}</Text>
+        <View style={styles.profilePictureContainer}>
+          <Image style={styles.profileImage}
+                 source={{uri: profileImageUrl}} />
+        </View>
+        <Text style={styles.fanCount}>{this._getFansText(numFans)}</Text>
+        <Text style={styles.bio}>{bio}</Text>
+      </View>
+    );
+  },
+
+  //TODO: This is more then likely something we want to api to handle
+  _getFansText: function(numFans) {
+    if (numFans == 0) {
+      return 'No fans :(';
+    }
+    else if (numFans == 1) {
+      return numFans + ' fan';
+    }
+    else {
+      return numFans + ' fans';
+    }
+  }
+
+});
+
+module.exports = ProfilePageBody;
--- ProfilePage.ios.js
+++ ProfilePage.ios.js
@@ -4,11 +4,10 @@ var React = require('react-native');
 var Unicycle = require('./Unicycle');
 var profileStore = require('./stores/ProfileStore');
 var MainScreenBanner = require('./MainScreenBanner');
+var ProfilePageBody = require('./Components/Profile/ProfilePageBody');
 
 var {
   View,
-  Text,
-  Image,
   StyleSheet,
   ActivityIndicatorIOS
 } = React
@@ -16,39 +15,6 @@ var {
 var styles = StyleSheet.create({
   profilePageContainer: {
     flex: 1
-  },
-  profileBodyContent: {
-    flex: 1,
-    backgroundColor: 'rgba(0,124,158,.2)'
-  },
-  fullName: {
-    textAlign: 'center',
-    fontSize: 30,
-    borderWidth: 1,
-    borderColor: '#007C9E',
-    borderRadius: 5,
-    margin: 5,
-    backgroundColor: 'white'
-  },
-  profilePictureContainer: {
-    flex: 2,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginBottom: 5
-  },
-  profileImage: {
-    width: 150,
-    height: 230
-  },
-  fanCount: {
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: '800'
-  },
-  bio: {
-    flex: 2,
-    alignSelf: 'auto',
-    margin: 30
   },
   spinnerContainer: {
     flex: 1,
@@ -81,7 +47,13 @@ var ProfilePage = React.createClass({
       content = <ProfilePageLoading/>
     }
     else {
-      content = this.renderProfilePageBody();
+      content = <ProfilePageBody
+                  firstName = {profileStore.getFirstName()}
+                  lastName = {profileStore.getLastName()}
+                  bio = {profileStore.getBio()}
+                  numFans = {profileStore.getNumFollowers()}
+                  profileImageUrl = {profileStore.getProfileImageUrl()}
+                />;
     }
 
     return (
@@ -92,39 +64,6 @@ var ProfilePage = React.createClass({
         { content }
       </View>
     );
-  },
-
-  renderProfilePageBody: function() {
-    var firstName = profileStore.getFirstName(),
-        lastName = profileStore.getLastName(),
-        bio = profileStore.getBio(),
-        numFans = profileStore.getNumFollowers(),
-        profileImageUrl = profileStore.getProfileImageUrl();
-
-    return (
-      <View style={styles.profileBodyContent}>
-        <Text style={styles.fullName}>{firstName} {lastName}</Text>
-        <View style={styles.profilePictureContainer}>
-          <Image style={styles.profileImage}
-                 source={{uri: profileImageUrl}} />
-        </View>
-        <Text style={styles.fanCount}>{this._getFansText(numFans)}</Text>
-        <Text style={styles.bio}>{bio}</Text>
-      </View>
-    );
-  },
-
-  //TODO: This is more then likely something we want to api to handle
-  _getFansText: function(numFans) {
-    if (numFans == 0) {
-      return 'No fans :(';
-    }
-    else if (numFans == 1) {
-      return numFans + ' fan';
-    }
-    else {
-      return numFans + ' fans';
-    }
   }
 
 });
