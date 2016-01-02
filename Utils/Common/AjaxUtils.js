9655cc36aaa50017e7bfc3971f91ee9db660f1f1
Jake Ruth <jruth@albany.edu>
2016-01-02 10:53:16 -0500

    Added private method to AjaxUtils to validate that the response received from the api was successful

--- Utils/Common/AjaxUtils.js
+++ Utils/Common/AjaxUtils.js
@@ -6,19 +6,29 @@ var prefix = require('superagent-prefix')('http://greedyapi.elasticbeanstalk.com
 var AjaxUtils = {
 
   ajax: function(url, data, onSuccessCallback, onFailureCallback) {
+    var that = this;
     request
      .post(url)
      .use(prefix)
      .send(data)
      .set('Accept', 'application/json')
      .end(function(err, res) {
-       if ((res !== undefined) && (res.ok)) {
+       if (that._isRequestSuccessful(res)) {
          onSuccessCallback(res);
        }
        else {
          onFailureCallback();
        }
     });
+  },
+
+  _isRequestSuccessful(res) {
+    return (
+      (res !== undefined) &&
+      (res.ok) &&
+      (res.body !== undefined) &&
+      (res.body.success)
+    );
   }
 
 }
