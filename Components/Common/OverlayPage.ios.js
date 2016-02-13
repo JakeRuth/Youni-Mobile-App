41360df92e3461c423509205e3e39473add06685
Jake Ruth <jruth@albany.edu>
2016-02-13 11:36:48 -0500

    Added auto scroll when pressing the comment input box on the post popup.  It autoscrolls so that the input box is in directly above the keyboard.

--- Components/Common/OverlayPage.ios.js
+++ Components/Common/OverlayPage.ios.js
@@ -8,7 +8,8 @@ var {
     View,
     StyleSheet,
     ScrollView,
-    Dimensions
+    Dimensions,
+    DeviceEventEmitter
 } = React;
 
 var styles = StyleSheet.create({
@@ -19,6 +20,19 @@ var styles = StyleSheet.create({
 
 var OverlayPage = React.createClass({
 
+    componentDidMount() {
+        DeviceEventEmitter.addListener('keyboardWillShow', () => {
+            this.setState({hasKeyboardBeenShownAtLeastOnce: true});
+        });
+    },
+
+    getInitialState: function() {
+        return {
+            hasKeyboardBeenShownAtLeastOnce: false,
+            bottomOfScrollViewPosition: null
+        };
+    },
+
     propTypes: {
         content: React.PropTypes.element.isRequired,
         onBackArrowPress: React.PropTypes.func.isRequired,
@@ -26,15 +40,41 @@ var OverlayPage = React.createClass({
     },
 
     render: function() {
+        var contentOffset = null;
+
+        if (this.state.hasKeyboardBeenShownAtLeastOnce) {
+            contentOffset = {
+                x: 0,
+                y: this._computePositionToScrollToWhenKeyboardDisplayed()
+            };
+        }
+
         return (
             <View style={styles.container}>
                 <MainScreenBanner title={this.props.bannerTitle}/>
-                <ScrollView>
-                    {this.props.content}
-                </ScrollView>
                 <BackArrow onPress={this.props.onBackArrowPress}/>
+
+                <ScrollView contentOffset={contentOffset}>
+
+                    {this.props.content}
+
+                    { /* This is a hacky way to compute the bottom of the scroll views position */ }
+                    <View
+                        style={{height: 250}}
+                        onLayout={(e) => {
+                        this.setState({
+                            bottomOfScrollViewPosition: e.nativeEvent.layout.y
+                        });
+                    }}/>
+
+                </ScrollView>
             </View>
         );
+    },
+
+    _computePositionToScrollToWhenKeyboardDisplayed: function() {
+        var spaceBetweenTopOfScreenAndTopOfKeyboard = (Dimensions.get('window').height - 300);
+        return this.state.bottomOfScrollViewPosition - spaceBetweenTopOfScreenAndTopOfKeyboard;
     }
 
 });
