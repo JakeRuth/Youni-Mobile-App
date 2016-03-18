0e3625ca476a3bbab66041b0f5e71d05c64da7df
Jake Ruth <jruth@albany.edu>
2016-03-17 21:32:43 -0400

    Changed the selector behavior for the login and sign up page so that if you click login, and your already on that page, it will remove all content and just show the video.  Same goes for the sign up view selector.

--- Components/LoginPage.ios.js
+++ Components/LoginPage.ios.js
@@ -108,7 +108,6 @@ var LoginPage = React.createClass({
 
   getInitialState: function() {
     return {
-      showIntroPage: true,
       inLoginView: false,
       isInSignUpView: false,
       isBackgroundImageLoading: true
@@ -152,20 +151,8 @@ var LoginPage = React.createClass({
 
           {this.renderContent(this.state.isBackgroundImageLoading)}
           <LoginSignupSelector
-            onLoginPress={() => {
-              this.setState({
-                showIntroPage: false,
-                isInSignUpView: false,
-                inLoginView: true
-              });
-            }}
-            onSignupPress={() => {
-              this.setState({
-                showIntroPage: false,
-                isInSignUpView: true,
-                inLoginView: false
-              });
-            }}/>
+            onLoginPress={this._onLoginSelectorPress}
+            onSignupPress={this._onSignUpSelectorPress}/>
           {appNameHeader}
 
         </Image>
@@ -234,6 +221,36 @@ var LoginPage = React.createClass({
         <Spinner/>
       </View>
     );
+  },
+
+  _onLoginSelectorPress: function() {
+    if (!this.state.inLoginView) {
+      this.setState({
+        isInSignUpView: false,
+        inLoginView: true
+      });
+    }
+    else {
+      this.setState({
+        isInSignUpView: false,
+        inLoginView: false
+      });
+    }
+  },
+
+  _onSignUpSelectorPress: function() {
+    if (!this.state.isInSignUpView) {
+      this.setState({
+        isInSignUpView: true,
+        inLoginView: false
+      });
+    }
+    else {
+      this.setState({
+        isInSignUpView: false,
+        inLoginView: false
+      });
+    }
   },
 
   _onForgotPasswordClick: function () {
