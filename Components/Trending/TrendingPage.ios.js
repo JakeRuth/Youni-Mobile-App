8bf26b91f6485aa595cd96fb85f5e4da1f18dd3a
Jake Ruth <jruth@albany.edu>
2016-07-26 12:06:28 -0400

    Removed the about trending icon from the trending page

--- Components/Trending/TrendingPage.ios.js
+++ Components/Trending/TrendingPage.ios.js
@@ -14,7 +14,6 @@ var {
   View,
   Text,
   StyleSheet,
-  TouchableHighlight,
   AlertIOS
 } = React;
 
@@ -36,13 +35,6 @@ var styles = StyleSheet.create({
     fontWeight: '500',
     textAlign: 'center',
     color: 'white'
-  },
-  aboutTrendingPageIconContainer: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    padding: 19,
-    paddingBottom: 0
   }
 });
 
@@ -103,39 +95,8 @@ var TrendingPage = React.createClass({
           allTimeFeed={TrendingFeedFilters.ALL_TIME}
           changeFeedSelector={(feed) => { this._changeFeedSelector(feed) }}/>
 
-        {this._renderAboutTrendingPageIcon()}
-
       </View>
     );
-  },
-
-  _renderAboutTrendingPageIcon: function() {
-    return (
-      <TouchableHighlight
-        style={styles.aboutTrendingPageIconContainer}
-        underlayColor='transparent'
-        onPress={this._aboutTrendingPageIconPress}>
-
-        <Icon
-          name='information-circled'
-          size={23}
-          color='white'/>
-
-      </TouchableHighlight>
-    );
-  },
-
-  _aboutTrendingPageIconPress: function() {
-    AlertIOS.alert(
-      'What is this?',
-      "Youni’s Trending pages feature the top and up-and-coming people at your university in real time! " +
-      "Daily Trending users have the highest scores for only that day. Weekly Trending users have the " +
-      "highest scores for only that week.  All Time Trending users have the highest scores overall at your university. " +
-      "To allow more users to be featured, the top 10 All Time Trending users won’t be featured in Daily or Weekly Trending.",
-      [
-        {text: 'Okay'}
-      ]
-    )
   },
 
   _changeFeedSelector: function(feed) {
