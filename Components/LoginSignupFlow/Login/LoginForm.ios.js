5fa7b0a31bb55b7c60ae9e925352f7ae654bf6a9
Jake Ruth <jruth@albany.edu>
2016-08-17 17:03:59 -0400

    Added a show password feature on the password input field when a user logs in

--- Components/LoginSignupFlow/Login/LoginForm.ios.js
+++ Components/LoginSignupFlow/Login/LoginForm.ios.js
@@ -2,7 +2,10 @@
 
 var React = require('react');
 var ReactNative = require('react-native');
+var Icon = require('react-native-vector-icons/MaterialIcons');
+
 var loginStore = require('../../../stores/LoginStore');
+
 var PrettyTouchable = require('../../Common/PrettyTouchable');
 var PrettyInput = require('../../Common/PrettyInput');
 
@@ -24,11 +27,23 @@ var styles = StyleSheet.create({
     marginBottom: 20
   },
   passwordInputContainer: {
-    marginBottom: 20
+    marginBottom: 20,
+    position: 'relative'
+  },
+  showPasswordIcon: {
+    position: 'absolute',
+    right: 5,
+    top: 8
   }
 });
 
 var LoginForm = React.createClass({
+
+  getInitialState: function() {
+    return {
+      showPassword: false
+    };
+  },
 
   render: function() {
     return (
@@ -55,13 +70,35 @@ var LoginForm = React.createClass({
             onTextChange={(password) => {
               loginStore.setPassword(password);
             }}
-            secureTextEntry={true}
+            secureTextEntry={!this.state.showPassword}
             clearTextOnFocus={true}
             placeholder='Password'/>
+          <Icon
+            style={styles.showPasswordIcon}
+            name={this._getShowPassIconName()}
+            size={25}
+            color='white'
+            onPress={this._toggleShowPasswordState}/>
         </View>
 
       </View>
     );
+  },
+
+  _getShowPassIconName: function() {
+    if (this.state.showPassword) {
+      return 'visibility-off';
+    }
+    else {
+      return 'visibility';
+    }
+  },
+
+  _toggleShowPasswordState: function() {
+    var originalState = this.state.showPassword;
+    this.setState({
+      showPassword: !originalState
+    });
   }
 
 });
