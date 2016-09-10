1fc4428611f4f34d2838aa8343096a00ba7968b0
Jake Ruth <jruth@albany.edu>
2016-09-10 13:17:11 -0400

    Attempt 2 at getting text invite to work.  Using a different plugin than the last commit, since the last plugin could only support sending a text to one number, not multiple.

--- Utils/Common/CommunicationUtils.js
+++ Utils/Common/CommunicationUtils.js
@@ -1,11 +1,71 @@
 'use strict';
 
-var Communications = require('react-native-communications');
+var ReactNative = require('react-native');
+var Composer = require('NativeModules').RNMessageComposer;
+
+var {
+  AlertIOS
+} = ReactNative;
 
 var CommunicationUtils = {
 
   sendText: function(phoneNumbers, message) {
-    Communications.default.text(phoneNumbers, message);
+    Composer.composeMessageWithArgs(
+      {
+        messageText: message,
+        subject: '',
+        recipients:  phoneNumbers
+      },
+      (result) => {
+        switch(result) {
+          case Composer.Sent:
+            AlertIOS.alert(
+              'Message sent!',
+              '',
+              {
+                text: 'Okay'
+              }
+            );
+            break;
+          case Composer.Cancelled:
+            AlertIOS.alert(
+              'Message sending was cancelled.',
+              '',
+              {
+                text: 'Okay'
+              }
+            );
+            break;
+          case Composer.Failed:
+            AlertIOS.alert(
+              'Failed to send the message.',
+              '',
+              {
+                text: 'Okay'
+              }
+            );
+            break;
+          case Composer.NotSupported:
+            AlertIOS.alert(
+              'This device does not support sending texts.',
+              '',
+              {
+                text: 'Okay'
+              }
+            );
+            break;
+          default:
+            AlertIOS.alert(
+              'An unexpected error occured.  If this continues please contact support@youniapp.com',
+              '',
+              {
+                text: 'Okay'
+              }
+            );
+            break;
+        }
+      }
+    );
   }
 
 };
